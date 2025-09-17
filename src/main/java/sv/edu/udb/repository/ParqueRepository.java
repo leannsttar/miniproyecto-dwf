3c411952ddb63fc2f37bac5436f38fea6f7b72ce
leannsttar <105887401+leannsttar@users.noreply.github.com>
2025-09-17 12:37:39 -0600

    feat(domain): add especie and parque entities with repositories

---
+++ src/main/java/sv/edu/udb/repository/ParqueRepository.java
@@ -0,0 +1,6 @@
+package sv.edu.udb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sv.edu.udb.domain.Parque;
+
+public interface ParqueRepository extends JpaRepository<Parque, Long> { }
